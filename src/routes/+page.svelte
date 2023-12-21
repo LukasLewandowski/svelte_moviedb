b00c8f231c6607d55fbcd3cc2fe2d77fe9c7d90e
Lukas Lewandowski <lukas.lewandowski@wallee.com>
2023-12-21 21:37:52 +0100

    playing around

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	let count = 0;
+
+	function incrementBtn() {
+		count += 1;
+	}
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+</nav>
+
+<h1>Number: {count}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={incrementBtn}> Increment </button>
+
+<style>
+	button {
+		background-color: rgb(147, 161, 201);
+	}
+</style>
